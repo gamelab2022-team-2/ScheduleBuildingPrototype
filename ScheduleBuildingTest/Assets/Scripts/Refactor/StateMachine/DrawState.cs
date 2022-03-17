a6208c18baddc022a9659d08ddb44a2c3164eae6
drouba <andreanne.chartrand@gmail.com>
2022-03-17 17:48:12 -0400

    CardCata scriptable object added. Card Class updated. Card objects for startdeck created. PROBLEM : loadData function in Card not loading data at game start

--- ScheduleBuildingTest/Assets/Scripts/Refactor/StateMachine/DrawState.cs
+++ ScheduleBuildingTest/Assets/Scripts/Refactor/StateMachine/DrawState.cs
@@ -29,21 +29,7 @@ public class DrawState : GameState
 
     public void Draw()
     {
-        while (_player.hand.Count < 5) // Draw while there are less than 5 cards in hand
-        {
-            if (_player.deck.Count > 0) // Draw from deck if there are more than 0 cards
-            {
-                Card drawnCard = _player.deck.Draw();
-                _player.hand.Add(drawnCard);
-            }
-            else _player.ReplenishDeck(); // if there are no more cards in the deck, add all discarded cards into the deck
-
-            // Not sure what this does (I assume it is animation)
-            // if (_player.deck.Count == 0)
-            // {
-            //     deck.SetActive(false);
-            // }
-        }
+        _player.DrawFromDeck();
     }
     
 
