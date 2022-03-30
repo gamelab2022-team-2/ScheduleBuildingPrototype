d467e11801915a246bb0c9b6f2f9a659b1d95ca5
Antoine Dufour-Blain <antoine.dufourblain@gmail.com>
2022-03-30 14:59:02 -0400

    __

--- ScheduleBuildingTest/Assets/Scripts/Game/Player.cs
+++ ScheduleBuildingTest/Assets/Scripts/Game/Player.cs
@@ -53,6 +53,11 @@ public class Player : MonoBehaviour
         {
             Debug.Log("   " + c.cardName);
         }
+        Debug.Log("DISCARD CONTAINS:");
+        foreach (Card c in discardPile.cards)
+        {
+            Debug.Log("   " + c.cardName);
+        }
         while (hand.Count < 5)
         {
 
