b0006b6d8d02f3309c725505c732d5d5d59354f1
PipeFlowWizard <elijahmon96@gmail.com>
2022-04-04 12:06:52 -0400

    small tweaks to scene and hand controller

--- ScheduleBuildingTest/Assets/HandController.cs
+++ ScheduleBuildingTest/Assets/HandController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
-    public Transform[] slots;
+    public Transform[] slots = new Transform[5];
 
     public float spacing = 2f;
 
